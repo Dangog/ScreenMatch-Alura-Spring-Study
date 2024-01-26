283563116ea0f93756b33792c987f1476ffeca40
Dangog <cajedanilo@gmail.com>
2024-01-25 23:53:37 -0300

    Added Principal Class

--- src/main/java/br/com/estudos/danilo/ScreenMatch/Principal/Principal.java
+++ src/main/java/br/com/estudos/danilo/ScreenMatch/Principal/Principal.java
@@ -1,16 +1,41 @@
 package br.com.estudos.danilo.ScreenMatch.Principal;
 
+import br.com.estudos.danilo.ScreenMatch.model.DadosSerie;
+import br.com.estudos.danilo.ScreenMatch.model.SeasonData;
+import br.com.estudos.danilo.ScreenMatch.service.ConsumoAPI;
+import br.com.estudos.danilo.ScreenMatch.service.JacksonDataConverter;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
     Scanner t = new Scanner(System.in);
 
+    private final String URL = "https://www.omdbapi.com/?t=";
+    private final String APIKey = "&apikey=1d559d70";
+    private ConsumoAPI principalConsume = new ConsumoAPI();
+    private JacksonDataConverter conv = new JacksonDataConverter();
 
     public void showMenu(){
         System.out.println("Digite a respectiva série para que faça a busca");
         var resp = t.nextLine();
 
+        var json = principalConsume.obterDados(URL + resp.replace(" ", "+") + APIKey);
+
+        DadosSerie convertedData = conv.getData(json, br.com.estudos.danilo.ScreenMatch.model.DadosSerie.class);
+        System.out.println(convertedData);
+
+        List<SeasonData> seasonsList = new ArrayList<>();
+
+		for (int i = 1; i<=convertedData.totalTemporadas(); i++){
+			json = principalConsume.obterDados(URL + resp.replace(" ", "+")+ "&season=" + i + APIKey);
+			SeasonData convertedSeasonData = conv.getData(json, SeasonData.class);
+			seasonsList.add(convertedSeasonData);
+		}
+		seasonsList.forEach(System.out::println);
 
 
     }
--- src/main/java/br/com/estudos/danilo/ScreenMatch/ScreenMatchApplication.java
+++ src/main/java/br/com/estudos/danilo/ScreenMatch/ScreenMatchApplication.java
@@ -1,17 +1,9 @@
 package br.com.estudos.danilo.ScreenMatch;
 
-import br.com.estudos.danilo.ScreenMatch.model.DadosSerie;
-import br.com.estudos.danilo.ScreenMatch.model.EpisodeData;
-import br.com.estudos.danilo.ScreenMatch.model.SeasonData;
-import br.com.estudos.danilo.ScreenMatch.service.ConsumoAPI;
-import br.com.estudos.danilo.ScreenMatch.service.JacksonDataConverter;
+import br.com.estudos.danilo.ScreenMatch.Principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenMatchApplication implements CommandLineRunner {
@@ -22,27 +14,9 @@ public class ScreenMatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
 
-		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=1d559d70");
-
-		JacksonDataConverter conv = new JacksonDataConverter();
-		DadosSerie convertedData = conv.getData(json, br.com.estudos.danilo.ScreenMatch.model.DadosSerie.class);
-		System.out.println(convertedData);
-
-		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=1d559d70");
-		EpisodeData convertedEpisodeData = conv.getData(json, EpisodeData.class);
-		System.out.println(convertedEpisodeData);
-
-		List<SeasonData> seasonsList = new ArrayList<>();
-
-		for (int i = 1; i<=convertedData.totalTemporadas(); i++){
-			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=1d559d70");
-			SeasonData convertedSeasonData = conv.getData(json, SeasonData.class);
-			seasonsList.add(convertedSeasonData);
-		}
-
-		seasonsList.forEach(System.out::println);
+		principal.showMenu();
 
 	}
 }
